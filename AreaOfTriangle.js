4b1cdbc3627d1e756afaf2545a01dce485f389f3
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-11-28 12:31:13 +0530

    Code Updated.

--- AreaOfTriangle.js
+++ AreaOfTriangle.js
@@ -1,1 +1,13 @@
+// Find the Area of the triangle when the all the sides length are Given.
+const side1 = 5;
+const side2 = 6;
+const side3 = 7;
+
+const perimeter = (side1 + side2 + side3) / 2 ;
+
+const Area = Math.sqrt(perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3) );
+
+let result = document.getElementById("txtdisplay").innerHTML = "The Area of the Triangle is : " + Area;
+
+
 
