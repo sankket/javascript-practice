fd5985340fe282485c95f060cb9333d447a4cbcf
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-08 14:02:42 +0530

    Code Updated

--- multipleOf.js
+++ multipleOf.js
@@ -1,1 +1,17 @@
 
+function multiplesOf(num) 
+{
+  let number = prompt("Enter the  Number");
+  if (num % 3 == 0 || num % 7 == 0) 
+  {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
+
+let result = document.getElementById("txtdisplay").innerHTML = "The Given Number is Multiple of 3 or 7 ? : "+ multiplesOf(number);
+
+// in javascript always remember to give valid function and make it understandable.
+// id name of the html element must be unique.
