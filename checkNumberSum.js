0e704da52460aef821ae080fae4faf27b62ef4f3
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-08 14:00:37 +0530

    Code Updated.

--- checkNumberSum.js
+++ checkNumberSum.js
@@ -1,1 +1,15 @@
+function checkNumberSumFifty(num1, num2){
+    let number1 = prompt("Enter the First Number");
+    let number2 = prompt("Enter the Second Number");
+    if(num1 == 50 || num2 == 50){
+        return true;
+    }else if(num1 + num2 == 50){
+        return true;
+    }
+    else{
+        return false;
+    }
+    // return ((num1 == 50 || num2 == 50) || (num1 + num2 == 50));
+ }
 
+let result = document.getElementById("txtdisplay").innerHTML = checkNumberSumFifty(number1, number2);
