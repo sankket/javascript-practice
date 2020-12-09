4a265091008b80691c0792dee507693d6d80af99
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-09 11:03:22 +0530

    Code Updated.

--- numberInRange.js
+++ numberInRange.js
@@ -1,1 +1,16 @@
+function numberInRange(num1, num2) 
+  {
+    let number1 = prompt("Enter the First Number");
+    let number2 = prompt("Enter the Second Number");
 
+  if ((num1 >= 50 && num2 <= 99) || (num1 >= 10 && num2 <= 99))
+  {
+    return true;
+  } 
+  else 
+  {
+    return false;
+  }
+}
+
+let result = document.getElementById("txtdisplay").innerHTML ="The Given Numbers Falls in range of 50 - 99 ? " +  numberInRange(number1, number2);
