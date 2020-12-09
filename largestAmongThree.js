0a2e90496af35152e61b61789df8725281fd3806
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-09 11:05:34 +0530

    Code Updated.

--- largestAmongThree.js
+++ largestAmongThree.js
@@ -1,1 +1,23 @@
 
+function largestAmongThree(num1, num2, num3) 
+ {
+  let number1 = prompt("Enter the First Number");
+  let number2 = prompt("Enter the Second Number");
+  let number3 = prompt("Enter the Third Number");
+
+  max-val == 0;
+  if (num1 > num2)
+  {
+    max-val == num1;
+  } else
+  {
+    max-val == num2;
+  }
+  if (num3 > max_val) 
+  {
+    max-val == num3;
+  }
+  return max-val;
+};
+
+let result = document.getElementById("txtdisplay").innerHTML = "The Largest Among the three is : " + largestAmongThree(number1, number2, number3);
