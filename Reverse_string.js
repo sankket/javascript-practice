d982dc7c48d411857790bd543c1cd3880851e4a4
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-12 10:45:16 +0530

    Code Updated.

--- Reverse_string.js
+++ Reverse_string.js
@@ -1,1 +1,14 @@
+function reverseString(str){
+    let split = str.split("");
 
+    let reverse = split.reverse();
+
+    let join = reverse.join("");
+
+    return join
+}
+
+let result = document.getElementById("txtdisplay").innerHTML = "Reverse of a String :" +reverseString("Sanket Godbole");
+
+
+// program for string reversal.
