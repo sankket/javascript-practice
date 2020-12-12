43fd6e7569cd9ac3e35960fbe0f0a4e0626cf4e6
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-12 10:42:06 +0530

    Code Updated.

--- CheckLastdigit.js
+++ CheckLastdigit.js
@@ -1,1 +1,16 @@
+function checkLastDigit(){
+let num1 = prompt("Enter the First Number");
+let num2 = prompt("Enter the Second Number");
+let num3 = prompt("Enter the Third Number");
 
+const result1 = num1 % 10;
+const result2 = num2 % 10;
+const result3 = num3 % 10;
+
+if(result1 == result2 && result1 == result3) {
+    let result = document.getElementById("txtdisplay").innerHTML=(`${num1}, ${num2} and ${num3} have the same last digit.`);
+}
+else {
+    let result = document.getElementById("txtdisplay").innerHTML=(`${num1}, ${num2} and ${num3} dont have the same last digit.`);
+}
+}
