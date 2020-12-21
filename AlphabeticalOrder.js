ba650b81344d1494f7de8a209e67a67e2a5bfb42
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-21 12:19:31 +0530

    Code Updated.

--- AlphabeticalOrder.js
+++ AlphabeticalOrder.js
@@ -1,1 +1,9 @@
+let str = prompt("Enter the String");
+
+function alphabetInOrder(str) { 
+
+    return str.split("").sort().join("");
+         
+}
+let result = document.getElementById("txtdisplay").innerHTML = alphabetInOrder(str);
 
