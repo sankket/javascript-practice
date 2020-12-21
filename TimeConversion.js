8e7163f965902ff2dadbc35393a6590dc5f49a32
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-21 12:18:31 +0530

    Code Updated.

--- TimeConversion.js
+++ TimeConversion.js
@@ -1,1 +1,10 @@
+let num1 = prompt("Enter the Number");
 
+function time_convert(num)
+ { 
+  var hours = Math.floor(num / 60);  
+  var minutes = num % 60;
+  return hours + "hours : " + minutes + "Minutes";         
+}
+
+let result = document.getElementById("txtdisplay").innerHTML = time_convert(num1);
