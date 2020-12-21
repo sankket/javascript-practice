137879cd196cc6d61991eb0a314aba3162bf4e6c
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-21 12:17:34 +0530

    Code Updated.

--- capitalLetter.js
+++ capitalLetter.js
@@ -1,1 +1,12 @@
+function capitalLetterOfString(str) 
+{
+    str = str.split(" ");
 
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+
+    return str.join(" ");
+}
+
+let result = document.getElementById("txtdisplay").innerHTML = capitalLetterOfString("my name is india");
