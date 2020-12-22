5ca76503124e19306364845142690ba2148cf542
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-22 16:03:41 +0530

    Code Updated.

--- ConcatStrings.js
+++ ConcatStrings.js
@@ -1,1 +1,10 @@
+const string1 = prompt('Enter a string1: ');
+const string2 = prompt('Enter a string2: ');
 
+function concatStrings(str1, str2) {
+    str1 = str1.substring(1, str1.length);
+    str2 = str2.substring(1, str2.length);
+    return str1 + str2;
+  }
+
+  alert(concatStrings(string1, string2));
