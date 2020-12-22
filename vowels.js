e331cabf883ee23a8bd66b2de310b325e44d76b6
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-22 16:01:52 +0530

    Code updated.

--- vowels.js
+++ vowels.js
@@ -1,1 +1,11 @@
+const string = prompt('Enter a string: ');
+
+function vowelsCount(str) { 
+    const count = str.match(/[aeiou]/gi).length; //gi is used for case sensitive searching.
+
+    return count;
+}
+
+
+let result = document.getElementById("txtdisplay").innerHTML ="The Count of the vowels in a given string : "+ vowelsCount(string);
 
