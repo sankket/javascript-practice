1c5ee69875caefa0e14f77de20c07ed1861d7e99
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-22 16:07:10 +0530

    Code Updated.

--- CurrentDay.js
+++ CurrentDay.js
@@ -1,1 +1,12 @@
+let currentdayOfYear = function(ob){
+today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1; //As January is 0.
+let yyyy = today.getFullYear();
 
+if(dd<10) dd='0'+dd;
+if(mm<10) mm='0'+mm;
+return (mm+ob+dd+ob+yyyy);
+};
+
+alert(currentdayOfYear("-"))
