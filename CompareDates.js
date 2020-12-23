bab41ab6ec2b5e154b70899468a9964709036d76
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-23 09:45:41 +0530

    Code Updated.

--- CompareDates.js
+++ CompareDates.js
@@ -1,1 +1,11 @@
+    function datesCompare(){
+    let dt1 = new Date('10/31/2020');  
+    let dt2 = new Date('11/25/2020'); 
+    if (dt1.getTime() < dt2.getTime()) 
+        document.write("date1 is lesser than date2"); 
+    else if (g1.getTime() > dt2.getTime()) 
+        document.write("date1 is greater than date2"); 
+    else
+        document.write("both are equal"); }
 
+    let result = document.getElementById("txtdisplay").innerHTML = datesCompare();
