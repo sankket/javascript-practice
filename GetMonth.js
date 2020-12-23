6dba57ee9ed1ed4428266ff6d99789e678155ca4
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-23 09:44:52 +0530

    Code Updated.

--- GetMonth.js
+++ GetMonth.js
@@ -1,1 +1,6 @@
+let monthName = function(date){
+    list = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+      return mlist[date.getMonth()];
+    };
 
+alert(monthName(new Date("31/12/2020")));
