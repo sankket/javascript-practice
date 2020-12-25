e1337c9a94f9fd9d2d52398222dbea624870497b
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-25 10:27:07 +0530

    Code Updated.

--- objectProperty.js
+++ objectProperty.js
@@ -1,1 +1,8 @@
+let objectProperty = { name: 'Sanket Godbole',
+    age: '22',
+    education: 'BE',
+    place : 'Aurangabad'
+}; 
+//alert(Object.values(object));
 
+alert(objectProperty.hasOwnProperty('age'));
