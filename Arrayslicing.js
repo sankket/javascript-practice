dcd1faebb29e404586f31d33a205a8ed65af7f8b
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-29 12:31:00 +0530

    Code Updated.

--- Arrayslicing.js
+++ Arrayslicing.js
@@ -1,1 +1,14 @@
+function arrayElementsSlicing(array,num){
+    if (array == null){
+        return 0;
+    }
+    if(num == null){
+        return array[0];
+    }
+    if(num < 0){
+        return [];
+    }
+    return array.slice(0 , num)
+};
 
+alert(arrayElementsSlicing([1,2,3,4,5],3))
