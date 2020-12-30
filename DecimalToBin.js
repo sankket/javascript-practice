722742e915a9ed9d77ce302fd72ed0f83a31bf89
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-30 09:48:38 +0530

    Code Updated.

--- DecimalToBin.js
+++ DecimalToBin.js
@@ -1,1 +1,11 @@
 
+let number1 = prompt("Enter the Number");
+function conversion(num){
+const hex = (number).toString(16);     
+const oct = (number).toString(8);      
+const bin = (number).toString(2);    
+
+return hex
+}
+
+alert(conversion(number1))
