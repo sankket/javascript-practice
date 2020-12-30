83be07e90ed77e83b54b621084e354c38fbdf5da
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-12-30 09:47:32 +0530

    Code Updated.

--- DecimalRoundup.js
+++ DecimalRoundup.js
@@ -1,1 +1,10 @@
+let number1 = prompt("Enter the Number");
+let decimal = prompt("Enter the decimals");
+function uptoDecimals(num, dec) {
+    if ((typeof num !== 'number') || (typeof d !== 'number'))
+      return false;
+           num = parseFloat(num) || 0;
+       return num.toFixed(dec);
+       }
 
+alert(uptoDecimals(number1, decimal))
