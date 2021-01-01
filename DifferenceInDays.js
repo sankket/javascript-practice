7edb7966aeba09d16a40ccf4a7bd5c7d5e8695e6
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-01-01 09:53:34 +0530

    Code Updated.

--- DifferenceInDays.js
+++ DifferenceInDays.js
@@ -1,1 +1,14 @@
+function diffInDates(){
+    date1 = new Date('11/28/2020');
+    date2 = new Date('11/21/2020');
+
+    const difference_time = Math.abs(date2 - date1);
+
+    const difference_days = Math.ceil(difference_time/(1000*3600*24)); //Milliseconds in a day.
+    
+    return difference_days;
+
+}
+
+alert(diffInDates())
 
