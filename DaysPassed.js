5ce26c822e63a1942bb0693d204a487c5c0e2311
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-01-09 09:07:53 +0530

    Code Updated.

--- DaysPassed.js
+++ DaysPassed.js
@@ -1,1 +1,10 @@
+function daysPassed(dt) {
+    var current = new Date(dt.getTime());
+    var previous = new Date(dt.getFullYear(), 0, 1);
+  
+    return Math.ceil((current - previous + 1) / 86400000); // ceil rounds up a number.
+  }
+
+alert(daysPassed(new Date(2020, 10, 27)))
+
 
