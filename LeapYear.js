8da6d68084bff863084acf8067bec4c30b74f6e3
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-01-09 09:06:46 +0530

    code Updated.

--- LeapYear.js
+++ LeapYear.js
@@ -1,1 +1,13 @@
+function isLeapYear(year) {
+    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+function daysOfYear(year) 
+{  
+  return isLeapYear(year) ? 366 : 365;
+}
 
+
+
+alert(daysOfYear(2020))
+
+// This Program shows whether the given year is Leap or Not.
