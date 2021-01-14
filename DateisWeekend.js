6a5a243d7709b4ac0c88d1bc905a5bb43edd334c
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-01-14 12:22:35 +0530

    Code Updated.

--- DateisWeekend.js
+++ DateisWeekend.js
@@ -1,1 +1,12 @@
+function dateisWeekend(){
+let givenDate=new Date("2020-1-1");
+let currentDay = givenDate.getDay();
+let dateIsInWeekend = (currentDay === 6) || (currentDay === 0);
+if(dateIsInWeekend==true){
+   alert("The given date "+givenDate+" is a Weekend");
+} else {
+   alert("The given date " +givenDate+"is a not a Weekend");
+}
+}
 
+alert(dateisWeekend())
