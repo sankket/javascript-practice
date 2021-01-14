942cb946050e2d186c531e299f117034e882d892
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-01-14 12:20:46 +0530

    Code Updated.

--- DaysInMonth.js
+++ DaysInMonth.js
@@ -1,1 +1,9 @@
+let num1 = prompt("Enter Month");
+let num2 = prompt("Enter Year");
 
+let getDaysInMonth = function(month,year) {
+  
+    return new Date(year, month+1, 0).getDate();
+  };
+
+alert("Days in given month : " +getDaysInMonth(num1,num2))
