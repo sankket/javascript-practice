6ce9ac637a471c97d6bf7d880f5bf784a39b8e83
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-01-19 10:21:24 +0530

    Code Updated.

--- ArrayUnion.js
+++ ArrayUnion.js
@@ -1,1 +1,10 @@
+function arrayUnion(){
+let array1 = [1,2,3,4,5];
+let array2 = [4,5,6,7];
+// Here merging elememnts of the two arrays.
 
+let array3 = [...array1,...array2];
+// ... is spread operator in javascript which expands array elements into individual elements.
+
+alert(array3);
+}
