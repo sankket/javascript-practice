45ae2bca438a1c502f2406aa82726f91afc10c40
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-01-19 10:20:07 +0530

    Code Updated.

--- ArrayClone.js
+++ ArrayClone.js
@@ -1,1 +1,9 @@
+function clonnedArray(){
+const original_array = [1,2,3,4,5];
 
+const clonned_array = [...original_array];
+
+alert("the Clonned array is : " + clonned_array);
+}
+
+clonnedArray();
