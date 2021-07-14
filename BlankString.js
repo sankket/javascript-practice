482e4a0bb4c251028391ff0f4cb220a3544fc1b3
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-07-14 16:05:13 +0530

    code updated.

--- BlankString.js
+++ BlankString.js
@@ -1,4 +1,5 @@
-blankString =  function(input) {
+//Return true when the function contains the blank string.
+var blankString =  function(input) {
     if (input.length === 0)
     return true;
     else 
