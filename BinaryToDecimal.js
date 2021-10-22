d9d0f70f6d08777da6393c3f3a2967fcc2080b37
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-10-22 14:31:55 +0530

    Code Updated.

--- BinaryToDecimal.js
+++ BinaryToDecimal.js
@@ -1,7 +1,8 @@
 function binaryToDecimal(){
-let binary = "111";
+let binary = "0111";
 let decimal = parseInt(binary, 2);
 alert(decimal);
 }
 
+alert("Program for conversion from Binary to Decimal")
 binaryToDecimal();
